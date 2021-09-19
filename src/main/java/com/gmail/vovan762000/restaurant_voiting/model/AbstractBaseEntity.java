2611c44a480b8a390090597093459aed002db306
vovan762000 <vovan762000@gmail.com>
2021-09-19 22:40:06 +0300

    add model User

---
+++ src/main/java/com/gmail/vovan762000/restaurant_voiting/model/AbstractBaseEntity.java
@@ -0,0 +1,45 @@
+package com.gmail.vovan762000.restaurant_voiting.model;
+
+import org.hibernate.Hibernate;
+
+public class AbstractBaseEntity {
+    public static final int START_SEQ = 100000;
+    protected Integer id;
+
+    protected AbstractBaseEntity() {
+    }
+
+    protected AbstractBaseEntity(Integer id) {
+        this.id = id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + ":" + id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || !getClass().equals(Hibernate.getClass(o))) {
+            return false;
+        }
+        AbstractBaseEntity that = (AbstractBaseEntity) o;
+        return id != null && id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id == null ? 0 : id;
+    }
+}
---
+++ src/main/java/com/gmail/vovan762000/restaurant_voiting/model/User.java
@@ -0,0 +1,4 @@
+package com.gmail.vovan762000.restaurant_voiting.model;
+
+public class User extends AbstractBaseEntity{
+}
